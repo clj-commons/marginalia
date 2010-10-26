c85bd874bcc49cd669f8286764969d9406f98e67
fogus <mefogus@gmail.com>
2010-10-26 15:51:15 -0400

    Setting up the project.clj file. Added templates

--- project.clj
+++ project.clj
@@ -1,4 +1,9 @@
-(defproject cljojo "1.0.0-SNAPSHOT"
-  :description "FIXME: write"
-  :dependencies [[org.clojure/clojure "1.2.0"]
-                 [org.clojure/clojure-contrib "1.2.0"]])
+(defproject cljojo "0.0.1"
+  :description "A port of Docco to Clojure."
+  :dependencies
+    [[org.clojure/clojure "1.2.0"]
+     [org.clojars.nakkaya/markdownj "1.0.2b4"]]
+  :dev-dependencies
+    [[lein-clojars "0.5.0-SNAPSHOT"]
+     [jline "0.9.94"]
+     [swank-clojure "1.2.1"]])
