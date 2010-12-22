7e9cea48524f523579751635a9ad400479ab312f
ninjudd <git@justinbalthrop.com>
2010-12-23 01:56:48 +0800

    add cake plugin

---
+++ src/marginalia/tasks.clj
@@ -0,0 +1,15 @@
+(ns marginalia.tasks
+  "# Cake plugin for running marginalia against your project.
+
+   ## Usage
+
+   1. In your project.clj, add marginalia to `:dev-dependencies` and `marginalia.tasks` to `:tasks`
+   2. Run `cake marg` from within your project directory."
+  (:use marginalia.core
+        [cake.core :only [deftask]]))
+
+(deftask marg
+  "Run marginalia against your project code."
+  "Optionally, you can pass a file or directories to control what documentation is generated and in what order."
+  {files :marg}
+  (run-marginalia files))
