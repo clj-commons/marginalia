63635acdd86159fdba8535b39d7b07cabb0a8f33
Brenton Ashworth <bashwort@gmail.com>
2010-12-23 07:47:56 +0800

    Example configuration for using MathJax to format Latex equations.

--- project.clj
+++ project.clj
@@ -1,16 +1,19 @@
 (defproject marginalia "0.2.1"
   :description "lightweight literate programming for clojure -- inspired by [docco](http://jashkenas.github.com/docco/)"
   :dependencies
-    [[org.clojure/clojure "1.2.0"]
-     [org.clojars.nakkaya/markdownj "1.0.2b4"]
-     [hiccup "0.3.0"]]
+  [[org.clojure/clojure "1.2.0"]
+   [org.clojars.nakkaya/markdownj "1.0.2b4"]
+   [hiccup "0.3.0"]]
   :dev-dependencies
-    [[lein-clojars "0.5.0-SNAPSHOT"]
-     [jline "0.9.94"]
-     [swank-clojure "1.2.1"]
-     ;;Needed for testing lein plugin
-     [hiccup "0.3.0"]
-     [org.clojars.nakkaya/markdownj "1.0.2b4"]
-     [marginalia "0.2.1"]]
+  [[lein-clojars "0.5.0-SNAPSHOT"]
+   [jline "0.9.94"]
+   [swank-clojure "1.2.1"]
+   ;;Needed for testing lein plugin
+   [hiccup "0.3.0"]
+   [org.clojars.nakkaya/markdownj "1.0.2b4"]
+   [marginalia "0.2.1"]]
   ;;Needed for testing cake plugin
-  :tasks [marginalia.tasks])
+  :tasks [marginalia.tasks]
+  ;;Needed for testing Latex equation formatting. You must download
+  ;;and install MathJax in you doc directory.
+  :marginalia {:javascript ["mathjax/MathJax.js"]})
