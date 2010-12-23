04a139fc4753d3e3b24d1123b09b2bc83d6c8508
zkim <zachary.kim@gmail.com>
2010-12-23 03:06:57 -0700

    Joined the two docstring strings on the marg cake task, bumped version to 0.2.1 in the comments.

--- src/marginalia/tasks.clj
+++ src/marginalia/tasks.clj
@@ -3,13 +3,13 @@
 
    ## Usage
 
-   1. In your project.clj, add `[marginalia \"0.2.0\"] to your `:dev-dependencies` and `marginalia.tasks` to `:tasks`
+   1. In your project.clj, add `[marginalia \"0.2.1\"] to your `:dev-dependencies` and `marginalia.tasks` to `:tasks`
    2. Run `cake marg` from within your project directory."
   (:use marginalia.core
         [cake.core :only [deftask]]))
 
 (deftask marg
-  "Run marginalia against your project code."
-  "Optionally, you can pass files or directories to control what documentation is generated and in what order."
+  "Run marginalia against your project code.
+   Optionally, you can pass files or directories to control what documentation is generated and in what order."
   {files :marg}
   (run-marginalia files))
