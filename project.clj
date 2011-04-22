d326e2fa84dcb5b81a7179abd5cb99dd0e25328d
Raynes <i@raynes.me>
2011-04-21 20:51:01 -0500

    Fix cake plugin.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject marginalia "0.5.0"
+(defproject marginalia "0.5.1-SNAPSHOT"
   :description "lightweight literate programming for clojure -- inspired by [docco](http://jashkenas.github.com/docco/)"
   :main marginalia.core
   :eval-in-leiningen true
