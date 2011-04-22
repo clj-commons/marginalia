70a0b2f6bc0c61c19bfdc6657be4b75f28b37c9e
Raynes <i@raynes.me>
2011-04-22 09:51:01 +0800

    Fix cake plugin.

--- src/marginalia/tasks.clj
+++ src/marginalia/tasks.clj
@@ -5,11 +5,13 @@
 
    1. In your project.clj, add `[marginalia \"<current version number>\"] to your `:dev-dependencies` and `marginalia.tasks` to `:tasks`
    2. Run `cake marg` from within your project directory."
-  (:use marginalia.core
-        [cake.core :only [deftask]]))
+  (:use cake.core))
 
 (deftask marg
   "Run marginalia against your project code.
    Optionally, you can pass files or directories to control what documentation is generated and in what order."
   {files :marg}
-  (run-marginalia files))
+  (bake
+   (:use marginalia.core) [files files]
+   (binding [marginalia.html/*resources* ""]
+     (run-marginalia files))))
