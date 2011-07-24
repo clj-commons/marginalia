1027d5bba341d2bc47dba818529c8ed5ee3e4546
dm3 <deadmoro3@gmail.com>
2011-07-25 06:31:49 +0800

    Fixed errors happening when parser encountered string or number literals outside of forms.
    .gitignore: added temporary file generated by `lein test`
    project.clj: dependency on vimclojure
    test/parse_test.clj: tests for situations causing errors pre-patch
    src/marginalia/parser.clj: literal handling during code block merges

--- test/parse_test.clj
+++ test/parse_test.clj
@@ -1,6 +1,8 @@
 (ns parse-test
   "This module does stuff"
-  (:require [clojure.string :as str]))
+  (:require [clojure.string :as str])
+  (:use clojure.test)
+  (:require marginalia.parser))
 
 
 ;; It seems that, in Clojure, block
@@ -13,3 +15,9 @@
    Does some other cool stuff too."
   (println (str "Hello World, " name "!")))
 
+(deftest test-inline-literals
+  (is (= (count (marginalia.parser/parse "(ns test)")) 1))
+  ;(is (= (count (marginalia.parser/parse "(ns test)\n123")) 1)) still failing
+  (is (= (count (marginalia.parser/parse "(ns test)\n123\n")) 1))
+  (is (= (count (marginalia.parser/parse "(ns test)\n\"string\"")) 1))
+  (is (= (count (marginalia.parser/parse "(ns test)\n\"some string\"")) 1)))
