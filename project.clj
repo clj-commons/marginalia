d0a0da4f83bc79aeab4e306c34be5679c8fef5ce
Fogus <mefogus@gmail.com>
2013-03-05 08:13:43 -0500

    Update project.clj

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "lightweight literate programming for clojure -- inspired by [docco](http://jashkenas.github.com/docco/)"
 ;;  :main marginalia.main
   :dependencies
-  [[org.clojure/clojure "1.4.0"]
+  [[org.clojure/clojure "1.5.0"]
    [org.clojure/tools.namespace "0.1.1"]
    [org.clojure/tools.cli "0.2.1"]
    [org.markdownj/markdownj "0.3.0-1.0.2b4"]]
