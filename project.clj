71645e7ff4cbc73211edf8b1a5194310bd6d6cb3
Gary Deer <gdeer81@gmail.com>
2015-08-26 13:32:07 -0500

    Updated project file

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "lightweight literate programming for clojure -- inspired by [docco](http://jashkenas.github.com/docco/)"
 ;;  :main marginalia.main
   :dependencies
-  [[org.clojure/clojure "1.5.1"]
+  [[org.clojure/clojure "1.7.0"]
    [org.clojure/clojurescript "0.0-2138"]
    [org.clojure/tools.namespace "0.1.1"]
    [org.clojure/tools.cli "0.2.1"]
