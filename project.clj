78a9ee69b47344707c08e23398e8c3ced30b8c65
Gary Deer <gdeer81@gmail.com>
2016-03-16 11:33:11 -0500

    Version 0.8.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject marginalia "0.8.1-SNAPSHOT"
+(defproject marginalia "0.8.1"
   :description "lightweight literate programming for clojure -- inspired by [docco](http://jashkenas.github.com/docco/)"
 ;;  :main marginalia.main
   :dependencies
