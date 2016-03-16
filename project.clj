71487629be8ef372bd70b43982a3bb11abae253d
Gary Deer <gdeer81@gmail.com>
2016-03-16 11:47:52 -0500

    updated project version

--- project.clj
+++ project.clj
@@ -1,13 +1,13 @@
-(defproject marginalia "0.8.1"
+(defproject marginalia "0.9.0"
   :description "lightweight literate programming for clojure -- inspired by [docco](http://jashkenas.github.com/docco/)"
 ;;  :main marginalia.main
   :dependencies
   [[org.clojure/clojure "1.7.0"]
-   [org.clojure/clojurescript "0.0-2138"]
-   [org.clojure/tools.namespace "0.1.1"]
-   [org.clojure/tools.cli "0.2.1"]
+   [org.clojure/clojurescript "1.7.228"]
+   [org.clojure/tools.namespace "0.2.10"]
+   [org.clojure/tools.cli "0.3.3"]
    [org.markdownj/markdownj "0.3.0-1.0.2b4"]
-   [de.ubercode.clostache/clostache "1.3.1"]]
+   [de.ubercode.clostache/clostache "1.4.0"]]
 
   :resource-paths ["vendor"]
 
