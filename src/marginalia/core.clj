cc712d16e4ac61a4f6453f9fa3de3fb9038c6f04
Dmitry Vasilyanov <vdmit11@gmail.com>
2018-05-05 11:33:20 +0300

    fix sorting in (find-processable-file-paths)
    The (parse-ns) function always returns a string,
    so this code:
        (sort-by #(->> % parse-ns second))
    actually sorts by the second character of the string.
    
    Seems like this (second) call is a rudiment from the past.
    And this commit removes it, and thus fixes the sorting.

--- src/marginalia/core.clj
+++ src/marginalia/core.clj
@@ -95,7 +95,7 @@
   (->> (io/file dir)
        (file-seq)
        (filter (partial processable-file? pred))
-       (sort-by #(->> % parse-ns second))
+       (sort-by parse-ns second)
        (map #(.getCanonicalPath %))))
 
 ;; ## Project Info Parsing
