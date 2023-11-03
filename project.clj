e2058b64fb2ed3a9667f77cea2df40103b54112b
Tim Macdonald <tim@tsmacdonald.com>
2023-11-03 15:13:05 +0000

    Upgrade to Clojure 1.11

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "lightweight literate programming for clojure -- inspired by [docco](http://jashkenas.github.com/docco/)"
 ;;  :main marginalia.main
   :dependencies
-  [[org.clojure/clojure "1.7.0"]
+  [[org.clojure/clojure "1.11.1"]
    [org.clojure/clojurescript "1.7.228"]
    [org.clojure/tools.namespace "0.2.10"]
    [org.clojure/tools.cli "0.3.3"]
