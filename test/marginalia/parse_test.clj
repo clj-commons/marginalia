13b31ed16d5f6886afaa689d534a2e3f475c83e2
Tim Macdonald <tim@tsmacdonald.com>
2024-05-03 15:45:44 +0100

    Fix fenceposting issue with docstring extraction

--- test/marginalia/parse_test.clj
+++ test/marginalia/parse_test.clj
@@ -6,7 +6,7 @@
 
 (deftest test-inline-literals
   (is (= (count (marginalia.parser/parse "(ns test)")) 1))
-  ;; (is (= (count (marginalia.parser/parse "(ns test)\n123")) 1)) ;; still failing
+  (is (= (count (marginalia.parser/parse "(ns test)\n123")) 1))
   (is (= (count (marginalia.parser/parse "(ns test)\n123\n")) 1))
   (is (= (count (marginalia.parser/parse "(ns test)\n\"string\"")) 1))
   (is (= (count (marginalia.parser/parse "(ns test)\n\"some string\"")) 1))
