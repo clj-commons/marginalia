ef8eb1f4b2764940625ab9c1d07332085c361a1e
Tim Macdonald <tim@tsmacdonald.com>
2024-05-06 14:51:53 +0100

    Kondo-fy latex

--- src/marginalia/latex.clj
+++ src/marginalia/latex.clj
@@ -1,46 +1,47 @@
 (ns marginalia.latex
   "Utilities for converting parse results into LaTeX."
-  (:require [clojure.string :as str]
-            [clostache.parser :as mustache]
-            [clojure.java.shell :as shell]
-            [clojure.string :as string])
-  (:use [marginalia.html :only [slurp-resource]]))
+  (:require
+   [clojure.java.shell :as shell]
+   [clojure.string :as str]
+   [clostache.parser :as mustache]
+   [marginalia.html :as html]))
 
+(set! *warn-on-reflection* true)
 
-(def template (slurp-resource "latex.mustache"))
+(def template "Mustache-format template to use for the document" (html/slurp-resource "latex.mustache"))
 
 ;; ## Namespace header calculation
 
-(def headers ["subsubsection" "subsection" "section"])
+(def ^:private ^clojure.lang.APersistentVector headers ["subsubsection" "subsection" "section"])
 
-(def section-re #"\\((sub){0,2}section)\s*\{")
+(def ^:private section-re #"\\((sub){0,2}section)\s*\{")
 
 (defn get-section-type
   "It calculates the suitable header for a namespace `ns`. For example,
-  if the generated output has subsections a section must be generated
-  for each namespace in the table of contents. If it has
-  subsubsections a subsection should be generated and so on."
+  if the generated output has subsections a section must be generated for each namespace in the table of contents. If
+  it has subsubsections a subsection should be generated and so on."
   [groups]
-  (let [docs (map :doc groups)
-        matches (->> docs (mapcat (partial re-seq section-re))
-                     (map second))
-        matches (or (seq matches) ["none"])
+  (let [matches        (->> groups
+                            (map :doc)
+                            (mapcat (partial re-seq section-re))
+                            (map second))
+        matches        (or (seq matches) ["none"])
         most-important (apply max (map #(.indexOf headers %) matches))
-        position (min (-> most-important inc)
-                      (-> (count headers) dec))]
+        position       (min (-> most-important inc)
+                            (-> (count headers) dec))]
     (nth headers (max position 0))))
 
-(defn get-header
-  [type ns]
-  (str "\\" type "{" ns "}"))
+(defn- get-header
+  [the-type ns']
+  (str "\\" the-type "{" ns' "}"))
 
 ;; ## Markdown to LaTeX conversion
 
-(def mark (string/join (repeat 3 "marginalialatex")))
+(def ^:private mark (str/join (repeat 3 "marginalialatex")))
 
-(def separator-marker (str "\n\n" mark "\n\n"))
+(def ^:private separator-marker (str "\n\n" mark "\n\n"))
 
-(def separator-marker-re (re-pattern (str "\n?\n?" mark "\n?\n?")))
+(def ^:private separator-marker-re (re-pattern (str "\n?\n?" mark "\n?\n?")))
 
 (defn invoke-pandoc
   "We use pandoc to convert `markdown-input` to LaTeX."
@@ -57,25 +58,25 @@
   "It applies pandoc in a string joined by `separator-marker`. The
   output is split using a regex created from `separator-marker`"
   [docs]
-  (let [joined (string/join separator-marker docs)
+  (let [joined (str/join separator-marker docs)
         output (invoke-pandoc joined)
-        result (string/split output separator-marker-re (count docs))]
+        result (str/split output separator-marker-re (count docs))]
     (assert (= (count docs) (count result))
             "The converted docs must have the same number")
     result))
 
-(defn to-latex
+(defn- to-latex
   [groups]
-  (map #(assoc-in %1 [:doc] %2) groups (md->latex (map :doc groups))))
+  (map #(assoc %1 :doc %2) groups (md->latex (map :doc groups))))
 
 ;; ## Code and doc extraction
 
-(defn docs-from-group [group]
+(defn- docs-from-group [group]
   (or (:docstring group)
       (and (= (:type group) :comment) (:raw group))
       ""))
 
-(defn code-from-group [group]
+(defn- code-from-group [group]
   (if (= (:type group) :code)
     (:raw group)
     ""))
@@ -89,17 +90,16 @@
 
 ;; ## Uberdoc generation
 
-(def convert-groups (comp to-latex extract-code-and-doc))
+(def ^:private convert-groups (comp to-latex extract-code-and-doc))
 
 (defn as-data-for-template
-  "
-   1. Converts each `groups` data to the format expected by the template.
-   1. Calculates the correct header level for namespaces."
+  "1. Converts each `groups` data to the format expected by the template.
+   2. Calculates the correct header level for namespaces."
   [project-metadata docs]
-  (let [namespaces (map #(update-in % [:groups] convert-groups) docs)
+  (let [namespaces   (map #(update % :groups convert-groups) docs)
         section-type (get-section-type (mapcat :groups namespaces))
-        namespaces (map #(assoc % :ns-header (get-header section-type (:ns %)))
-                        namespaces)]
+        namespaces   (map #(assoc % :ns-header (get-header section-type (:ns %)))
+                          namespaces)]
     {:namespaces namespaces :project project-metadata}))
 
 (defn uberdoc-latex
