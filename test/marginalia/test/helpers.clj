3a4650e0c8b8f2943166f77f1dd65c19615e1f45
Tim Macdonald <tim@tsmacdonald.com>
2024-05-06 14:52:35 +0100

    Kondo-fy test helpers

--- test/marginalia/test/helpers.clj
+++ test/marginalia/test/helpers.clj
@@ -1,10 +1,11 @@
 (ns marginalia.test.helpers
   (:require
-   [clojure.java.io :refer [file delete-file]]
+   [clojure.java.io :as io]
    [clojure.test :refer :all]
-   [marginalia.core :as core]
+   [marginalia.core :as marginalia]
    [marginalia.html :as html]))
 
+(set! *warn-on-reflection* true)
 
 ;; copied from http://clojuredocs.org/clojure_contrib/clojure.contrib.io/delete-file-recursively
 ;; N.B. that Raynes's filesystem library could possibly replace this,
@@ -12,17 +13,21 @@
 (defn delete-file-recursively
   "Delete file f. If it's a directory, recursively delete all its contents. Raise an exception if any deletion fails unless silently is true."
   [f & [silently]]
-  (let [f (file f)]
+  (let [f (io/file f)]
     (when (.isDirectory f)
       (doseq [child (.listFiles f)]
         (delete-file-recursively child silently)))
-    (delete-file f silently)))
+    (io/delete-file f silently)))
 
-(defn find-clojure-file-paths [source-dir]
-  (core/find-processable-file-paths source-dir #(re-find #"clj$" %)))
+(defn find-clojure-file-paths
+  "Returns a seq of string paths for Clojure files in the given `source-dir`"
+  [source-dir]
+  (marginalia/find-processable-file-paths source-dir #(re-find #"clj$" %)))
 
-(defn files-in [dir]
-  (seq (.listFiles (file dir))))
+(defn files-in
+  "A seq of `File`s in the `dir`"
+  [dir]
+  (seq (.listFiles (io/file dir))))
 
 (defmacro with-project
   "Runs assertions in the context of a project set up for testing in the `test_projects` directory.
@@ -38,23 +43,21 @@
                        output files and test project metadata
    * `tests` - assertions to be run after the output has been produced"
   [project-name doc-generator & tests]
-  (let [project (file "test_projects" project-name)
-        test-project-src (str (file project "src"))
-        test-project-target (str (file project "docs"))
-        test-metadata {
-           :dependencies [["some/dep" "0.0.1"]]
-           :description "Test project"
-           :name "test"
-           :dev-dependencies []
-           :version "0.0.1"
-         }]
+  (let [project             (io/file "test_projects" project-name)
+        test-project-src    (str (io/file project "src"))
+        test-project-target (str (io/file project "docs"))
+        test-metadata       {:dependencies [["some/dep" "0.0.1"]]
+                             :description "Test project"
+                             :name "test"
+                             :dev-dependencies []
+                             :version "0.0.1"}]
 
     `(do
        (delete-file-recursively ~test-project-target true)
-       (.mkdirs (file ~test-project-target))
+       (.mkdirs (io/file ~test-project-target))
        (binding [html/*resources* ""]
          (~doc-generator ~test-project-src ~test-project-target ~test-metadata))
-       (let [~'number-of-generated-pages (count (files-in ~test-project-target))]
+       (let [~'number-of-generated-pages (count (io/files-in ~test-project-target))]
          ;; We need to `deftest` in order for test runners (e.g. `lein test`) to pick up failures
          (deftest ~(gensym (str project-name "-"))
            ~@tests))
